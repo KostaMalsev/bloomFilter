a60df030477f39fc523fd6dd47a11833dbaae40f
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2023-02-02 07:02:04 +0200

    Update bitwize.java

--- bitwize.java
+++ bitwize.java
@@ -1,2 +1,30 @@
-
-
+public class MyClass {
+    
+    int unuseBitdMap=0;//ones in size of map in constructor
+    
+    static int invert(int n) {
+        int mask = (Integer.highestOneBit(n) << 1) - 1;
+        return (~n & mask);  
+    }
+    
+    public static void main(String args[]) {
+     
+     unuseBitdMap =  initBitMap(4);
+     
+     System.println(Integer.toBinaryString(unuseBitdMap));
+     setBit(3);
+     System.println(Integer.toBinaryString(unuseBitdMap));
+        
+    }
+    
+    private void setBit(int pos)
+    {
+        unusedMap |= 1 << pos;
+    }
+    
+    private void initBitMap(int numOfParams)
+    {
+        return ~(~0 << numOfParams);
+    }
+    
+}
