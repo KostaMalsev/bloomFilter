eaca50409d7c1ffa6fd4fedd262cb2d63037aa77
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2023-02-02 07:29:52 +0200

    Update bitwize.java

--- bitwize.java
+++ bitwize.java
@@ -35,3 +35,30 @@ public class MyClass {
     }
     
 }
+
+
+static int findPosition(int n)
+    {
+        if (!isPowerOfTwo(n))
+            return -1;
+ 
+        int i = 1, pos = 1;
+ 
+        // Iterate through bits of n till we find a set bit
+        // i&n will be non-zero only when 'i' and 'n' have a set bit
+        // at same position
+        while ((i & n) == 0) {
+            // Unset current bit and set the next bit in 'i'
+            i = i << 1;
+ 
+            // increment position
+            ++pos;
+        }
+ 
+        return pos;
+    }
+    
+    static boolean isPowerOfTwo(int n)
+    {
+        return (n > 0 && ((n & (n - 1)) == 0)) ? true : false;
+    }
