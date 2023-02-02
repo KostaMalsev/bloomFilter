23d3cf5666db4c6eecfdd77d1a6819c83471a4a4
Kosta Malsev <51965607+KostaMalsev@users.noreply.github.com>
2023-02-02 07:17:31 +0200

    Update bitwize.java

--- bitwize.java
+++ bitwize.java
@@ -1,28 +1,35 @@
 public class MyClass {
     
-    int unuseBitdMap=0;//ones in size of map in constructor
+    //int unuseBitdMap=0;//ones in size of map in constructor
     
-    static int invert(int n) {
+    private static int invert(int n) 
+    {
         int mask = (Integer.highestOneBit(n) << 1) - 1;
         return (~n & mask);  
     }
     
     public static void main(String args[]) {
      
-     unuseBitdMap =  initBitMap(4);
+     long unuseBitdMap = 0;//initBitMap(4);
      
-     System.println(Integer.toBinaryString(unuseBitdMap));
-     setBit(3);
-     System.println(Integer.toBinaryString(unuseBitdMap));
+     System.out.println(Integer.toBinaryString(unuseBitdMap));
+     //unuseBitdMap = unSetBit(3,unuseBitdMap);
+     //unuseBitdMap = unSetBit(1,unuseBitdMap);
+     unuseBitdMap = unSetBit(10,unuseBitdMap);
+
+     System.out.println(Integer.toBinaryString(unuseBitdMap));
+     
+     System.out.println("Length of max int: "+ (Integer.toBinaryString(2147483647).length()));//max bits are 31.for long is 62 bits for bitmask.
         
     }
     
-    private void setBit(int pos)
+    private static int unSetBit(int pos,int unusedMap)
     {
         unusedMap |= 1 << pos;
+        return unusedMap;
     }
     
-    private void initBitMap(int numOfParams)
+    private static int initBitMap(int numOfParams)
     {
         return ~(~0 << numOfParams);
     }
